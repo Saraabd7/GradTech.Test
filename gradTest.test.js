8e12bcbb1fd33216303775703a1411efebea20b8
sarahabd7 <sarah_majdii12@hotmail.com>
2020-09-06 16:02:59 +0100

    All tests passed!

--- gradTest.test.js
+++ gradTest.test.js
@@ -1,33 +1,23 @@
-
-function getChildArray (menu, parent) {
-  for (var j = 0; j < menu.length; j++) {
-    if (menu[j].title == parent) {
-      return menu[j].data
-    }
-  }
-  var newParent = { title: parent, data: [] }
-  menu.push(newParent)
-  return menu[menu.length - 1].data
-}
-
 function createMenuData (data) {
-  var menu = []
+  const result = []
+  data.forEach(text => {
+    var items = text.split('/')
 
-  for (var i = 0; i < data.length; i++) {
-    var parentAndChild = data[i].split('/')
-    var childs = getChildArray(menu, parentAndChild[0])
-    if (parentAndChild[1] != null) {
-      childs.push(parentAndChild[1])
+    if (items.length > 1) {
+      var exists = result.find(element => {
+        return element.title === items[0]
+      })
+      if (exists) {
+        exists.data.push(items[1])
+      } else {
+        result.push({
+          title: items[0],
+          data: [items[1]]
+        })
+      }
     }
   }
-
-  var result = []
-  for (i = 0; i < menu.length; i++) {
-    if (menu[i].data.length != 0) {
-      result.push(menu[i])
-    }
-  }
-
+  )
   return result
 }
 
@@ -43,14 +33,20 @@ describe('menu Data Generator', () => {
       'parent3/parent3child1',
       'parent4'
     ]
-
     const expectedResult = [
       {
         title: 'parent1',
         data: ['parent1child', 'parent1child2', 'parent1child3']
       },
-      { title: 'parent2', data: ['parent2child', 'parent2child2'] },
-      { title: 'parent3', data: ['parent3child1'] }
+
+      {
+        title: 'parent2',
+        data: ['parent2child', 'parent2child2']
+      },
+      {
+        title: 'parent3',
+        data: ['parent3child1']
+      }
     ]
 
     const actualResult = createMenuData(data)
