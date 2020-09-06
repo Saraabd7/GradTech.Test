bc377c89edae3f804e274f04d73ea4023efa503e
sarahabd7 <sarah_majdii12@hotmail.com>
2020-09-06 03:01:44 +0100

    All tests passed!!

--- gradTest.test.js
+++ gradTest.test.js
@@ -1,63 +1,59 @@
 
-function getChildArray(menu, parent) {
-  
+function getChildArray (menu, parent) {
   for (var j = 0; j < menu.length; j++) {
     if (menu[j].title == parent) {
-      return menu[j].data;
+      return menu[j].data
     }
   }
- 
   var newParent = { title: parent, data: [] }
-  menu.push(newParent);
-  return menu[menu.length - 1].data;
+  menu.push(newParent)
+  return menu[menu.length - 1].data
 }
 
-function createMenuData(data) {
-
-  var menu = [];
+function createMenuData (data) {
+  var menu = []
 
   for (var i = 0; i < data.length; i++) {
-    var parentAndChild = data[i].split("/");
-    var childs = getChildArray(menu, parentAndChild[0]);
+    var parentAndChild = data[i].split('/')
+    var childs = getChildArray(menu, parentAndChild[0])
     if (parentAndChild[1] != null) {
-      childs.push(parentAndChild[1]);
+      childs.push(parentAndChild[1])
     }
   }
 
- 
-  var result = [];
+  var result = []
   for (i = 0; i < menu.length; i++) {
     if (menu[i].data.length != 0) {
-      result.push(menu[i]);
+      result.push(menu[i])
     }
   }
 
-  return result;
+  return result
 }
 
-describe("menu Data Generator", () => {
-    it("creates correct data structure ", () => {
-      const data = [
-        "parent1/parent1child",
-        "parent1/parent1child2",
-        "parent2/parent2child",
-        "parent2/parent2child2",
-        "parent1/parent1child3",
-        "parent3",
-        "parent3/parent3child1",
-        "parent4"
-      ];
-  
-      const expectedResult = [
-        {
-          title: "parent1",
-          data: ["parent1child", "parent1child2", "parent1child3"]
-        },
-        { title: "parent2", data: ["parent2child", "parent2child2"] },
-        { title: "parent3", data: ["parent3child1"] }
-      ];
-  
-      const actualResult = createMenuData(data);
-      expect(actualResult).toMatchObject(expectedResult);
-    });
-  });
+describe('menu Data Generator', () => {
+  it('creates correct data structure ', () => {
+    const data = [
+      'parent1/parent1child',
+      'parent1/parent1child2',
+      'parent2/parent2child',
+      'parent2/parent2child2',
+      'parent1/parent1child3',
+      'parent3',
+      'parent3/parent3child1',
+      'parent4'
+    ]
+
+    const expectedResult = [
+      {
+        title: 'parent1',
+        data: ['parent1child', 'parent1child2', 'parent1child3']
+      },
+      { title: 'parent2', data: ['parent2child', 'parent2child2'] },
+      { title: 'parent3', data: ['parent3child1'] }
+    ]
+
+    const actualResult = createMenuData(data)
+    expect(actualResult).toMatchObject(expectedResult)
+  })
+})
